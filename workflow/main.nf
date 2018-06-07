54b35b23645d5d97c38f017b514ac41ee12eedba
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2018-06-07 17:06:56 -0500

    Initial commit

---
+++ workflow/main.nf
@@ -0,0 +1,79 @@
+#!/usr/bin/env nextflow
+
+// Path to an input file, or a pattern for multiple inputs
+// Note - $baseDir is the location of this workflow file main.nf
+
+// Define Input variables
+params.bcl = "$baseDir/../test_data/*.tar"
+params.designFile = "$baseDir/../test_data/design.csv"
+params.genome = 'GRCm38'
+
+// Define List of Files
+tarList = Channel.fromPath( params.bcl )
+
+
+// Define regular variables
+
+
+process checkDesignFile {
+
+  publishDir "$baseDir/output/design", mode: 'copy'
+
+  input:
+
+  params.designFile
+
+  output:
+
+  file("design.csv") into designPaths
+
+  script:
+
+  """
+  python $baseDir/scripts/check_design.py -d $params.designFile
+  """
+}
+
+
+process untarBCL {
+  tag "$tar"
+
+  publishDir "$baseDir/output/bcl", mode: 'copy'
+
+  input:
+
+  file tar from tarList
+
+  output:
+
+  file("*") into bclPaths
+
+  script:
+
+  """
+  
+  tar -xvf $tar
+  """
+}
+
+
+process mkfastq {
+
+  tag "${bcl.baseName}"
+  publishDir "$baseDir/output/fastq/${bcl.baseName}", mode: 'copy' 
+
+  input:
+
+  val bcl from bclPaths
+  file designPaths
+
+  output:
+
+  file("**/outs/fastq_path/**/*") into fastqPaths
+
+  script:
+
+  """
+  cellranger mkfastq --id="${bcl.baseName}" --run=$bcl --csv=$designPaths
+  """
+}
---
+++ workflow/scripts/check_design.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+'''Check if design file is correctly formatted and matches files list.'''
+
+import argparse
+import logging
+import pandas as pd
+
+EPILOG = '''
+For more details:
+        %(prog)s --help
+'''
+
+# SETTINGS
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+logger.propagate = False
+logger.setLevel(logging.INFO)
+
+
+def get_args():
+    '''Define arguments.'''
+
+    parser = argparse.ArgumentParser(
+        description=__doc__, epilog=EPILOG,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('-d', '--design',
+                        help="The design file to run QC (tsv format).",
+                        required=True)
+
+    args = parser.parse_args()
+    return args
+
+
+def check_design_headers(design):
+    '''Check if design file conforms to sequencing type.'''
+
+    # Default headers
+    design_template = [
+        'Lane',
+        'Sample',
+        'Index']
+
+    design_headers = list(design.columns.values)
+
+    # Check if headers
+    logger.info("Running header check.")
+
+    missing_headers = set(design_template) - set(design_headers)
+
+    if len(missing_headers) > 0:
+        logger.error('Missing column headers: %s', list(missing_headers))
+        raise Exception("Missing column headers: %s" % list(missing_headers))
+    
+    return design
+
+def main():
+    args = get_args()
+    design = args.design
+
+    # Create a file handler
+    handler = logging.FileHandler('design.log')
+    logger.addHandler(handler)
+
+    # Read files as dataframes
+    design_df = pd.read_csv(args.design, sep=',')
+
+    # Check design file
+    new_design_df = check_design_headers(design_df)
+
+    new_design_df.to_csv('design.csv', header=True, sep=',', index=False)
+
+if __name__ == '__main__':
+    main()
