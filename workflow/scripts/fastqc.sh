2f5a2b473d1a8d6d0346a7065edddfe6d7a8e0f8
s181706 <jonathan.gesell@utsouthwestern.edu>
2019-04-11 17:51:07 -0500

    Updated fastqc for better multithreading, updated module lists.

--- workflow/scripts/fastqc.sh
+++ workflow/scripts/fastqc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-find . -name '*.fastq.gz' | awk '{printf("fastqc \"%s\"\n", $0)}' | parallel -j 25 --verbose
-#find . -name '*fastqc.*' | xargs -I '{}' mv '{}' ./
-
-fastqc --version |& grep 'FastQC v' | sed -n -e 's/^FastQC v//p' > version_fastqc.txt  
+find . -name '*.fastq.gz' | awk '{printf("fastqc \"%s\"\n", $0)}' | parallel -j `grep -c ^processor /proc/cpuinfo` --verbose
+#find . -name '*fastqc.*' | xargs -I '{}' mv '{}' ./ 
+#for i in `ls *.fastq.gz`;
+#do echo "fastqc ${i}";
+#done | parallel -j `grep -c ^processor /proc/cpuinfo` --verbose;
+fastqc --version | grep 'FastQC v' | sed -n -e 's/^FastQC v//p' > version_fastqc.txt  
