0c7feeadddf176c1c584438901de0f21c5d8a263
Jeremy Mathews <Jeremy.Mathews@utsouthwestern.edu>
2019-07-19 15:05:40 -0500

    Fix paths

--- workflow/tests/test_fastqc.py
+++ workflow/tests/test_fastqc.py
@@ -7,8 +7,10 @@ import os
 import glob
 
 test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
-		'/../../test/mkfastq/'
+		'/../../test/mkfastq/**/outs/fastq_path/**/**/'
 
 @pytest.mark.fastqc
 def test_fastqc_output():
-    assert os.path.exists(os.path.join(test_output_path, glob.glob('*.fastq.gz', recursive=True)))
+    fastqcs = glob.glob('/../../test/mkfastq/*/outs/fastq_path/*/*/*.fastq.gz', recursive=True)
+    for f in fastqcs
+    assert os.path.exists(os.path.join(test_output_path, f))
