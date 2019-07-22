e981dbffd59e4fd64dcf90bc4c82cfced47e7a33
Jeremy Mathews <Jeremy.Mathews@utsouthwestern.edu>
2019-07-22 10:50:25 -0500

    make literal

--- workflow/tests/test_check_design.py
+++ workflow/tests/test_check_design.py
@@ -6,10 +6,9 @@ from io import StringIO
 import os
 
 test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
-                '/../output/misc/checkDesignFile/**/'
+                "/../output/misc/checkDesignFile/**/"
 
 @pytest.mark.design
 def test_design():
-    design_file = os.path.join(test_output_path, 'design.checked.csv')
-    print(design_file)
+    design_file = os.path.join(test_output_path, "design.checked.csv")
     assert os.path.exists(design_file)
