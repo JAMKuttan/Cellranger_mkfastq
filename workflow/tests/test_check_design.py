f761cc1525e215c408a365e971d13abd39144836
Jeremy Mathews <Jeremy.Mathews@utsouthwestern.edu>
2019-07-22 10:56:37 -0500

    comment out design

--- workflow/tests/test_check_design.py
+++ workflow/tests/test_check_design.py
@@ -6,9 +6,9 @@ from io import StringIO
 import os
 
 test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
-                "/../output/misc/checkDesignFile/**/"
+                '/../output/misc/checkDesignFile/**/'
 
 @pytest.mark.design
 def test_design():
-    design_file = os.path.join(test_output_path, "design.checked.csv")
+    design_file = os.path.join(test_output_path, 'design.checked.csv')
     assert os.path.exists(design_file)
