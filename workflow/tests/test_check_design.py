908ca9d872ce755141f903c45567adb70a3e28aa
Jeremy Mathews <Jeremy.Mathews@utsouthwestern.edu>
2019-07-22 11:26:34 -0500

    hardcode check design test

--- workflow/tests/test_check_design.py
+++ workflow/tests/test_check_design.py
@@ -6,7 +6,7 @@ from io import StringIO
 import os
 
 test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
-                '/../output/misc/checkDesignFile/**/'
+                '/../output/misc/checkDesignFile/run/'
 
 @pytest.mark.design
 def test_design():
