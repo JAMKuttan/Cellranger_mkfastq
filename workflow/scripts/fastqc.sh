8cdd120851c1c4b34107512402db188e912f5d6d
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2019-08-14 20:58:25 -0500

    Update fastqc.sh

--- workflow/scripts/fastqc.sh
+++ workflow/scripts/fastqc.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#fastqc.sh
+#*
+#* --------------------------------------------------------------------------
+#* Licensed under MIT (https://git.biohpc.swmed.edu/BICF/Astrocyte/cellranger_mkfastq/blob/develop/LICENSE)
+#* --------------------------------------------------------------------------
+#*
+
 
 find . -name '*.fastq.gz' | awk '{printf("fastqc \"%s\"\n", $0)}' | parallel -j $(grep -c ^processor /proc/cpuinfo) --verbose
 #find . -name '*fastqc.*' | xargs -I '{}' mv '{}' ./ 
