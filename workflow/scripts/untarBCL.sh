1c96b063aecab7a15cdaea366f53b512574ba704
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2019-08-14 20:58:46 -0500

    Update untarBCL.sh

--- workflow/scripts/untarBCL.sh
+++ workflow/scripts/untarBCL.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #untarBCL.sh
+#countDesign.sh
+#*
+#* --------------------------------------------------------------------------
+#* Licensed under MIT (https://git.biohpc.swmed.edu/BICF/Astrocyte/cellranger_mkfastq/blob/develop/LICENSE)
+#* --------------------------------------------------------------------------
+#*
 
 usage() {
   echo "-t  --tar file"
