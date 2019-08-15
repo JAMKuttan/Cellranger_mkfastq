f9fc35346a4bad6fe3313c1024585a814f296959
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2019-08-14 21:00:24 -0500

    Update fastqc.sh

--- workflow/scripts/fastqc.sh
+++ workflow/scripts/fastqc.sh
@@ -7,7 +7,3 @@
 #*
 
 find . -name '*.fastq.gz' | awk '{printf("fastqc \"%s\"\n", $0)}' | parallel -j $(grep -c ^processor /proc/cpuinfo) --verbose
-#find . -name '*fastqc.*' | xargs -I '{}' mv '{}' ./ 
-#for i in `ls *.fastq.gz`;
-#do echo "fastqc ${i}";
-#done | parallel -j `grep -c ^processor /proc/cpuinfo` --verbose;
